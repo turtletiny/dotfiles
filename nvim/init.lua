b57f725589a6a2147863b9e0ff199ca56d4aa9ee
Daniel <daniel.lukins1@gmail.com>
2026-09-18 18:48:21 +1000

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -147,7 +147,7 @@ do
 
     -- Can switch between these as you prefer
     virtual_text = false, -- Text shows up at the end of the line
-    virtual_lines = true, -- Text shows up underneath the line, with virtual lines
+    virtual_lines = false, -- Text shows up underneath the line, with virtual lines
 
     -- Auto open the float, so you can easily read the errors when jumping with `[d` and `]d`
     jump = {
